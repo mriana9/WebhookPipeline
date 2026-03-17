039371f3d297a5dae86f4984abec258d96874c7c
mriana9 <mariana.algafy@sharkod>
2026-03-17 02:14:54 +0200

    feat: define jobs table schema using Drizzle ORM

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,16 +1,28 @@
-import { pgTable, timestamp, uuid, text, jsonb } from "drizzle-orm/pg-core";
+import { pgTable, timestamp, uuid, text, jsonb } from 'drizzle-orm/pg-core';
 
 export const pipelines = pgTable('pipelines', {
   id: uuid('id').primaryKey().defaultRandom().notNull(),
   name: text('name').notNull().unique(),
   sourceKey: uuid('source_key').defaultRandom().notNull().unique(),
   //(TRANSFORM, FILTER, ENRICH)
-  actionType: text('action_type').notNull(), 
+  actionType: text('action_type').notNull(),
   destinationUrl: text('destination_url').notNull(),
-  actionConfig: jsonb('action_config'), 
+  actionConfig: jsonb('action_config'),
   createdAt: timestamp('created_at').notNull().defaultNow(),
   updatedAt: timestamp('updated_at')
     .notNull()
     .defaultNow()
     .$onUpdate(() => new Date()),
+});
+
+export const jobs = pgTable('jobs', {
+  id: uuid('id').primaryKey().defaultRandom().notNull(),
+  pipeline_id: uuid('pipeline_id')
+    .notNull()
+    .references(() => pipelines.id, { onDelete: 'cascade' }), //ondeleie pipline delete all his jobs
+  payload: jsonb('payload'),
+  //pending, processing, completed, failed
+  status: text('status').notNull(),
+  createdAt: timestamp('created_at').notNull().defaultNow(),
+  updatedAt: timestamp('updated_at').notNull().defaultNow(),
 });
